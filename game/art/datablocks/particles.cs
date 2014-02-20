6aef92587400f5aeb4dd7a1a31e68d5ce8498da7
Andrew Mac <andrewmac@gmail.com>
2014-02-20 13:22:41 -0400

    Updated samples to include Particle demo.

--- game/art/datablocks/particles.cs
+++ game/art/datablocks/particles.cs
@@ -241,3 +241,48 @@ datablock ParticleEmitterNodeData(TorchFireEmitterNode)
 {
    timeMultiple = 1;
 };
+
+datablock ParticleData(PhysicsParticle)
+{
+   textureName = "core/art/defaultParticle";
+   dragCoefficient = 0.498534;
+   gravityCoefficient = 0;
+   inheritedVelFactor = 0.499022;
+   constantAcceleration = 0.0;
+   lifetimeMS = 12500;
+   lifetimeVarianceMS = 500;
+   useInvAlpha = true;
+   spinRandomMin = -360;
+   spinRandomMax = 360;
+   spinSpeed = 1;
+
+   colors[0] = "0.992126 0.00787402 0.0314961 1";
+   colors[1] = "1 0.834646 0 1";
+   colors[2] = "1 0.299213 0 1";
+   colors[3] = "0.732283 1 0 1";
+   
+   sizes[0] = 0;
+   sizes[1] = 0.497467;
+   sizes[2] = 0.73857;
+   sizes[3] = 0.997986;
+   
+   times[0] = 0.0;
+   times[1] = 0.247059;
+   times[2] = 0.494118;
+   times[3] = 1;
+   
+   animTexName = "core/art/defaultParticle";
+};
+
+datablock ParticleEmitterData(PhysicsEmitter)
+{
+   ejectionPeriodMS = "50";
+   ejectionVelocity = "1";
+   velocityVariance = "0";
+   ejectionOffset = "0.2";
+   thetaMax = "40";
+   particles = "PhysicsParticle";
+   blendStyle = "ADDITIVE";
+   softParticles = "0";
+   softnessDistance = "1";
+};
